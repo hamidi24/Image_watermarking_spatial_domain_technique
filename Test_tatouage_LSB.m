9623b3a87495f2f62473d5268b5a15903e3bf8e3
hamidi24 <hamidi.mohamed24@google.com>
2024-04-03 16:06:37 +0200

    init

---
+++ Test_tatouage_LSB.m
@@ -0,0 +1,5 @@
+clear all;close all;clc;
+lsb_embed;
+lsb_recover;
+%disp('Le psnr ', psnr);
+fprintf('Le PSNR : %.2f\n', psnr);
---
+++ lsb_recover.m
@@ -0,0 +1,35 @@
+
+
+% save start time
+start_time=cputime;
+
+% Lire l'image tatouée
+file_name='lsb_watermarked.bmp';
+watermarked_image=imread(file_name);
+
+% définir la taille de l'image tatouée
+Mw=size(watermarked_image,1);	%Height
+Nw=size(watermarked_image,2);	%Width
+
+% Restituer les bits du poids faible de l'image tatouée afin d'extraire les bits de la marque
+for ii = 1:Mw
+    for jj = 1:Nw
+        watermark(ii,jj)=bitget(watermarked_image(ii,jj),1);
+    end
+end
+
+% Redimensionner la marque
+watermark=256*double(watermark);
+
+% Temps de traitement (d'execution)
+elapsed_time=cputime-start_time,
+
+% Lire la marque originale (en cas d'utilisation de PSNR pour la comparer
+% avec celle extraite)
+file_name='copyright.bmp';
+orig_watermark=imread(file_name);
+
+% scale and display recovered watermark
+figure(3)
+imshow(watermark,[])
+title('Recovered Watermark')
